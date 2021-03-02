26e78e24560c992c742a7b10e60ff0725cea1952
Atreya <atreyaksinha@gmail.com>
2021-03-02 12:27:25 -0800

    add model, Portfolio.cs

---
+++ TradingWebApplication/Models/Portfolio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TradingWebApplication.Models
+{
+    public class Portfolio
+    {
+        public int UserID { get; set; }
+        public string Alpaca_Key { get; set; }
+        public string Alpaca_Secret_Key { get; set; }
+        public double Portfolio_Value { get; set; }
+        public double Profit_Loss { get; set; }
+    }
+}

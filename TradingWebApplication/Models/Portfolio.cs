f359df29c23a791b2cac50ac7dd7499d5727854b
Atreya <atreyaksinha@gmail.com>
2021-03-06 01:17:32 -0800

    add constructor

--- TradingWebApplication/Models/Portfolio.cs
+++ TradingWebApplication/Models/Portfolio.cs
@@ -7,10 +7,17 @@ namespace TradingWebApplication.Models
 {
     public class Portfolio
     {
-        public int UserID { get; set; }
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
         public string Alpaca_Key { get; set; }
         public string Alpaca_Secret_Key { get; set; }
         public double Portfolio_Value { get; set; }
         public double Profit_Loss { get; set; }
+
+        public Portfolio()
+        {
+
+        }
     }
 }

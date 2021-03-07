04f67174ec82e539ecc32c4e7eed691c3eba5daa
Atreya <atreyaksinha@gmail.com>
2021-03-07 04:05:46 -0800

    create MVC for portfolio

--- TradingWebApplication/Data/ApplicationDbContext.cs
+++ TradingWebApplication/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TradingWebApplication.Models;
 
 namespace TradingWebApplication.Data
 {
@@ -12,5 +13,6 @@ namespace TradingWebApplication.Data
             : base(options)
         {
         }
+        public DbSet<TradingWebApplication.Models.Portfolio> Portfolio { get; set; }
     }
 }

31833621d90dd09ad6a1165730183cd8c70ea478
Atreya <atreyaksinha@gmail.com>
2021-03-29 16:59:08 -0700

    complete DayTrade page view

---
+++ TradingWebApplication/Data/Migrations/20210329174757_DatabaseCreate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TradingWebApplication.Data.Migrations
+{
+    public partial class DatabaseCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
